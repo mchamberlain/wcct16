11d21c5a287402175da38fa797b725c17211633d
Morné Chamberlain <morne.chamberlain@gmail.com>
2016-09-08 12:05:57 +0200

    Added failing integration test.

--- tests/integration-tests/test-integration-sample.php
+++ tests/integration-tests/test-integration-sample.php
@@ -19,4 +19,16 @@ class IntegrationTests extends \WP_UnitTestCase {
 		// Replace this with some actual testing code.
 		$this->assertTrue( true );
 	}
+
+	function test_wcct_save_option() {
+		$expected = 'river-club';
+
+		wcct_save_option('venue', $expected);
+
+		$actual = get_option('wcct_venue');
+		$this->assertEquals(
+			$expected,
+			$actual,
+			'Option expected to equal ' . $expected . ' but instead was ' . $actual);
+	}
 }
