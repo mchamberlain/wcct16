8b48d41544fc1d9c3715b1c9982daa00c078fc52
Morné Chamberlain <morne.chamberlain@gmail.com>
2016-09-08 11:43:57 +0200

    Added test for `wcct_get_option`.

--- tests/unit-tests/test-unit-sample.php
+++ tests/unit-tests/test-unit-sample.php
@@ -1,10 +1,10 @@
 <?php
 namespace WCCT;
- 
+
 use phpmock\phpunit\PHPMock;
- 
+
 class UnitTests extends \PHPUnit_Framework_TestCase{
-    use PHPMock;
+	use PHPMock;
 
 	/**
 	 * A single example test.
@@ -12,5 +12,9 @@ class UnitTests extends \PHPUnit_Framework_TestCase{
 	function test_sample() {
 		// Replace this with some actual testing code.
 		$this->assertTrue( true );
+	}
+
+	function test_wcct_get_option() {
+		$this->assertEquals('river-club', wcct_get_option('venue'));
 	}
 }
